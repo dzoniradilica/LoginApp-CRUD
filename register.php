1a9c06c5a8c8f6980044e3150e64c4c57d5228de
dzoniradilica <dzonidzonatan2@gmail.com>
2026-02-20 19:39:17 +0100

    Added logic for matching passwords

---
+++ register.php
@@ -0,0 +1,84 @@
+<?php
+    include "db.php";
+
+    $errors = "";
+
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $hash_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+        if(password_verify($_POST['confirm_password'], $hash_password)) {
+            echo "Topic";
+        } else {
+            $errors = "Passwords don't match!";
+        }
+    }
+?>
+
+<!-- Include Header -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body class="register">
+    <nav>
+        <ul>
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+    
+            <!-- When the user is logged in -->
+            <li>
+                <a href="admin.php">Admin</a>
+            </li>
+            <li>
+                <a href="logout.php">Logout</a>
+            </li>
+    
+            <!-- When the user is not logged in -->
+            <li>
+                <a href="register.php">Register</a>
+            </li>
+            <li>
+                <a href="login.php">Login</a>
+            </li>
+        </ul>
+    </nav>
+    
+<div class="container">
+    <div class="form-container">
+        <form method="POST" action="">
+            <h2>Create your Account</h2>
+
+            <!-- Error message placeholder -->
+            <p style="color:red">
+                <?php echo $errors; ?>
+            </p>
+
+            <label for="username">Username:</label>
+            <input placeholder="Enter your username" type="text" name="username" required>
+
+            <label for="email">Email:</label>
+            <input placeholder="Enter your email" type="email" name="email" required>
+
+            <label for="password">Password:</label>
+            <input placeholder="Enter your password" type="password" name="password" required>
+
+            <label for="confirm_password">Confirm Password:</label>
+            <input placeholder="Confirm your password" type="password" name="confirm_password" required>
+
+            <input type="submit" value="Register">
+        </form>
+    </div>
+</div>
+    
+</body>
+</html>
+
+<!-- Include Footer -->
